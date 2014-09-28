bdc049520e7f83a0ffa5656021c837d4f5e876c2
slstevens <sean.stevens.123@gmail.com>
2014-09-28 12:40:50 +0100

    Added customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,2 +1,13 @@
 class Customer
+
+	attr_reader :name, :number, :streetaddress, :postcode
+
+	def initialize(name, number, streetaddress, postcode)
+		@name = name
+		@number = number
+		@streetaddress = streetaddress
+		@postcode = postcode
+	end
+
+	
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,10 +2,18 @@ require 'customer'
 
 describe Customer do
 
-	let(:customer) {Customer.new}
-	let(:order) {double :order}
-	let(:dish) {double :dish}
+	let(:customer) {Customer.new('Sean', '07702847862', '16 Copse Wood Way', 'HA6 2UE')}
 
-	it "should be able to add a dish to the order" do
+	it "should have a name" do
+		expect(customer.name).to eq "Sean"
+	end
+
+	it "should have a phone number" do
+	end
+
+	it "should have a delivery street address" do
+	end
+
+	it "should have a delivery postcode" do
 	end
 end
